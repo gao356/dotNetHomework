b480fb233a5efae5f176937929de5a255af91d7c
gao <976292406@qq.com>
2021-02-28 12:48:19 +0800

    this is a test

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -6,7 +6,7 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! This is a test!");
         }
     }
 }
