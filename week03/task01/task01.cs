96cd8a5431fd8cf0eb42f86742a5c68e3d94307c
gao <976292406@qq.com>
2021-03-12 16:17:53 +0800

    week03

---
+++ week03/task01/task01.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace task01
+{
+    public interface Shape
+    {
+        double GetArea(); //计算面积
+        bool IsValid(); //判断图形是否合法
+    }
+
+    public class Triangle : Shape
+    {
+        private double baseLength; //底边长度
+        private double height; //高
+
+        public Triangle(double _base, double height)
+        {
+            this.baseLength = _base;
+            this.height = height;
+        }
+
+        public double BaseLength { set; get; }
+        public double Height { set; get; }
+
+        public double GetArea()
+        {
+            return baseLength * height * 0.5;
+        }
+
+        public bool IsValid()
+        {
+            return (baseLength > 0 && height > 0);
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        private double baseLength; // 底边长度
+        private double height; // 高度
+
+        public Rectangle(double b, double h)
+        {
+            baseLength = b;
+            height = h;
+        }
+
+        public virtual double BaseLength { set; get; }
+        public virtual double Height { set; get; }
+
+        public double GetArea()
+        {
+            return baseLength * height;
+        }
+
+        public virtual bool IsValid()
+        {
+            return (baseLength > 0 && height > 0);
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public Square(double side) : base(side, side) { }
+
+        public override double BaseLength {
+            get {
+                return this.BaseLength;
+            } set {
+                this.BaseLength = value;
+                this.Height = value;
+            }   
+        }
+        public override double Height {
+            get
+            {
+                return this.Height;
+            }
+            set
+            {
+                this.BaseLength = value;
+                this.Height = value;
+            }
+        }
+
+        public override bool IsValid()
+        {
+            return (BaseLength > 0 && Height > 0 && (BaseLength == Height));
+        }
+    }
+
+    class MainClass
+    {
+        static void Main()
+        {
+            Shape square = new Square(4);
+            Console.WriteLine("The area of this square is {0}", square.GetArea());
+            Shape triangle = new Triangle(3, 4);
+            Console.WriteLine("The area of this triangle is {0}", triangle.GetArea());
+            Shape rec = new Rectangle(3, 4);
+            Console.WriteLine("The area of this rectangle is {0}", rec.GetArea());
+        }
+    }
+}
---
+++ week03/task02/task02.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using task01;
+
+
+namespace task02
+{
+    public enum ShapeType
+    { 
+        TRIANGLE,
+        RECTANGLE,
+        SQUARE
+    }
+
+    class ShapeFactory
+    {
+        public static Shape GetShape(ShapeType type, double b, double h)
+        {
+            if (type == ShapeType.RECTANGLE) return new Rectangle(b, h);
+            else if (type == ShapeType.SQUARE) return new Square(b);
+            else if (type == ShapeType.TRIANGLE) return new Triangle(b, h);
+            else return null;
+        }
+    }
+
+    class MainClass
+    { 
+        static void Main()
+        {
+            Random rd = new Random();
+            int t = rd.Next(0, 2);
+            ShapeType type;
+            if (t == 0) type = ShapeType.RECTANGLE;
+            else if (t == 1) type = ShapeType.SQUARE;
+            else type = ShapeType.TRIANGLE;
+            double b = rd.NextDouble() * 10;
+            double h = rd.NextDouble() * 10;
+
+            double totalArea = 0;
+            for (int i = 0; i < 10; i++) 
+            {
+                Shape shape = ShapeFactory.GetShape(type, b, h);
+                totalArea += shape.GetArea();
+            }
+
+            Console.WriteLine("Total area is {0}.", totalArea);
+        }
+    }
+}
