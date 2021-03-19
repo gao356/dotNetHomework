6031892ce5659645ec078de3b0714b4c21e79985
gao <976292406@qq.com>
2021-03-19 16:58:12 +0800

    week04.

---
+++ week04/clock/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Threading;
+
+namespace clock
+{
+    public class Clock
+    {
+        public int Hour { get; set; }
+        public int Min { get; set; }
+        public int Sec { get; set; }
+        public int AlarmHour { get; set; }
+        public int AlarmMin { get; set; }
+        public int AlarmSec { get; set; }
+
+        public event TickHandler th;
+        public event AlarmHandler ah;
+
+        public Clock()
+        {
+            DateTime nowTime = System.DateTime.Now;
+            Hour = nowTime.Hour;
+            Min = nowTime.Minute;
+            Sec = nowTime.Second;
+        }
+
+        public Clock(int h, int m, int s)
+        {
+            Hour = h;
+            Min = m;
+            Sec = s;
+
+            if (Hour >= 24 || Hour <= -1) Hour = 0;
+            if (Sec >= 60 || Sec <= -1) Sec = 0;
+            if (Min >= 60 || Min <= -1) Min = 0;
+        }
+
+        public void SetAlarmTime(int h, int m, int s)
+        {
+            AlarmHour = h;
+            AlarmMin = m;
+            AlarmSec = s;
+
+            if (AlarmHour >= 24 || AlarmHour <= -1) AlarmHour = 0;
+            if (AlarmSec >= 60 || AlarmSec <= -1) AlarmSec = 0;
+            if (AlarmMin >= 60 || AlarmMin <= -1) AlarmMin = 0;
+        }
+
+        private void AddSec()
+        {
+            Sec++;
+            if (Sec == 60)
+            {
+                Sec = 0;
+                Min++;
+            }
+            if (Min == 60)
+            {
+                Min = 0;
+                Hour++;
+            }
+            if (Hour == 24) Hour = 0;
+        }
+
+        public void StartClock()
+        {
+            while (true) 
+            {
+                Thread.Sleep(1000);
+                this.AddSec();
+                Tick();
+                if (Hour == AlarmHour && Min == AlarmMin && Sec == AlarmSec)
+                {
+                    Alarm();
+                }
+            }
+        }
+
+        public void Tick()
+        {
+            th(this);
+        }
+
+        public void Alarm()
+        {
+            ah(this);
+        }
+    }
+
+    public delegate void TickHandler(Clock c);
+    public delegate void AlarmHandler(Clock c);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock c = new Clock(10, 10, 10);
+
+            // 注册事件
+            c.th += OnTick;
+            c.ah += OnAlarm;
+            c.SetAlarmTime(10, 10, 20);
+
+            // 启动闹钟
+            c.StartClock();
+        }
+
+        static void OnTick(Clock c)
+        {
+            Console.WriteLine("{0}:{1}:{2}", c.Hour, c.Min, c.Sec);
+        }
+
+        static void OnAlarm(Clock c)
+        {
+            Console.WriteLine("The alarm is ringing! Now is {0}:{1}:{2}", c.Hour, c.Min, c.Sec);
+        }
+    }
+}
---
+++ week04/linkedlist/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace week04
+{
+
+    public class Node<T>
+    {
+        public Node<T> NextNode { get; set; }
+        public T Data { get; set; }
+
+
+        public Node(T data)
+        {
+            Data = data;
+            NextNode = null;
+        }
+    }
+
+    public class LinkedList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void Add(T data)
+        {
+            if (head == null)
+            {
+                head = new Node<T>(data);
+                tail = head;
+            }
+            else
+            {
+                tail.NextNode = new Node<T>(data);
+                tail = tail.NextNode;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            if (head == null) return;
+            else 
+            {
+                Node<T> temp = head;
+                while (temp != null)
+                {
+                    action(temp.Data);
+                    temp = temp.NextNode;
+                }
+            }
+        }
+
+    }
+
+
+    class Programe
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            list.ForEach(d => Console.WriteLine(d));
+
+            int sum = 0;
+            list.ForEach(d => sum += d);
+            Console.WriteLine("The sum of the datas in the linkedlist is {0}", sum);
+
+            int max = 0;
+            int min = 0;
+            int count = 0;
+            list.ForEach(data =>
+            {
+                if (count == 0)
+                {
+                    max = data;
+                    min = data;
+                    count++;
+                }
+                else 
+                {
+                    if (data > max) max = data;
+                    if (data < min) min = data;
+                    count++;    
+                }
+            });
+
+            Console.WriteLine("The max of the datas in the linkedlist is {0}", max);
+            Console.WriteLine("The min of the datas in the linkedlist is {0}", min);
+        }
+    }
+}
