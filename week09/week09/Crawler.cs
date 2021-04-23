84ffaf4af8c03b3027a3831add08f77bd1134c64
gao <976292406@qq.com>
2021-04-23 18:54:35 +0800

    bugs fixed

--- week09/week09/Crawler.cs
+++ week09/week09/Crawler.cs
@@ -31,12 +31,6 @@ namespace week09
 
                 if (current == null || count > 10) break;
 
-                // 只爬取指定网址
-                if(!current.Contains(CertainWebsite))
-                {
-                    urls[current] = true;
-                    continue;
-                }
 
                 // 只有爬取指定类型网页才解析爬取下一级URL
                 if (current.Contains(".html") || current.Contains(".aspx") || current.Contains(".jsp") || count <= 1) { }
@@ -50,7 +44,7 @@ namespace week09
                 string html = DownLoad(current); // 下载
                 urls[current] = true;
                 count++;
-                Parse(html);//解析,并加入新的链接
+                Parse(html, current);//解析,并加入新的链接
                 MessageBox.Show("爬行" + current + "结束");
 
             }
@@ -70,12 +64,12 @@ namespace week09
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                MessageBox.Show(ex.Message + ":" + url);
                 return "";
             }
         }
 
-        public void Parse(string html)
+        public void Parse(string html, string currentUrl)
         {
             string strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+[""']";
             MatchCollection matches = new Regex(strRef).Matches(html);
@@ -83,6 +77,27 @@ namespace week09
             {
                 strRef = match.Value.Substring(match.Value.IndexOf('=') + 1)
                           .Trim('"', '\"', '#', '>');
+
+                // 只爬取指定网址
+                if (!strRef.Contains(CertainWebsite))
+                {
+                    continue;
+                }
+                // MessageBox.Show(strRef);
+
+                //相对路径转绝对
+                if (strRef.Contains(".."))
+                {
+                    if (currentUrl.EndsWith(@"/"))
+                    {
+                        strRef = currentUrl + strRef.Replace("..", "");
+                    }
+                    else 
+                    {
+                        strRef = currentUrl.Substring(0, currentUrl.LastIndexOf('/') + 1) + strRef.Replace("..", "");
+                    } 
+                }
+
                 if (strRef.Length == 0) continue;
                 if (urls[strRef] == null) urls[strRef] = false;
             }
